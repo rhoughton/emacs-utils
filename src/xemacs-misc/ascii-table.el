4a69bd30689c8c7be774ae8dcaebb7be1330bfc8
houghton <>
1995-11-13 16:43:28 +0000

    Misc packages

---
+++ src/xemacs-misc/ascii-table.el
@@ -0,0 +1,35 @@
+;; ----------
+;; Snarfed from Steve Humble
+(defun ascii-table (new)
+  "Show the buffer *Ascii Table* or make one.
+Make a new one if NEW (or prefix arg) is non-nil."
+  (interactive "P")
+  (let ((buf "*Ascii Table*")
+        (c 0)
+        (header "char  8  10  16%s")
+        c64 c128 c192)
+    (if (and (not new) (get-buffer buf))
+        (display-buffer buf)
+      (with-output-to-temp-buffer buf
+        (message "Building ascii table...")
+        ;;Silly spacing, but it looks better that way.
+        (princ (format header "    "))
+        (princ (format header "       "))
+        (princ (format header "      "))
+        (princ (format header "\n"))
+        (while (< c 64)
+          (setq c64 (+ c 64)
+                c128 (+ c 128)
+                c192 (+ c 192))
+          (princ (format
+                  ;;Yes, it's possible to take out the remaining
+                  ;;spaces in the format string by changing the field
+                  ;;widths, but that would make it even more illegible.
+                  "%3s%4o%4d%4x %6s%4o%4d%4x %9s%4o%4d%4x %8s%4o%4d%4x\n"
+                  (key-description (char-to-string c)) c c c
+                  (key-description (char-to-string c64)) c64 c64 c64
+                  (key-description (char-to-string c128)) c128 c128 c128
+                  (key-description (char-to-string c192)) c192 c192 c192))
+          (setq c (1+ c)))
+        (message "Building ascii table...done."))))
+)
