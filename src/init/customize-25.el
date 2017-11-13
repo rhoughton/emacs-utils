e85c17ada3443cc49be0962e780507f33360d07c
Paul Houghton <paul4hough@gmail.com>
2017-11-12 18:31:31 -0700

    add emacs 25 support - bout time :)

---
+++ src/init/customize-25.el
@@ -0,0 +1,30 @@
+;; 2017-11-12 (cc) Paul Houghton <paul4hough@gmail.com>
+
+;; Customization variables
+
+(custom-set-variables
+ '(cvs-display-full-path nil)
+ '(cvs-edit-confirm (quote changed))
+ '(debug-on-error t)
+ '(dired-verify-modtimes nil)
+ '(display-time-mode t)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(explicit-bash-args (quote ("-li")))
+ '(js-indent-level 2)
+ '(Man-notify-method (quote newframe))
+ '(mode-compile-make-options "" t)
+ '(perl-indent-level 2)
+ '(save-place t nil (saveplace))
+ '(sh-basic-offset 2)
+ '(shell-file-name "/bin/bash")
+ '(sh-indentation 2)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ )
+(custom-set-faces
+;; '(default ((t (:size "8pt" :family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 80 :width normal))))
+;; '(default ((t (:size "8pt" :family "Courier New"))))
+;; '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+;; '(default ((t (:size "8pt" :family "Courier New"))))
+ '(region ((t (:background "cyan")))))
